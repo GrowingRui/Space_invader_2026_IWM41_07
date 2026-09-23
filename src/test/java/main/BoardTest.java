ee7b2007fe2a376ab1014000208e3c9bcc85c4ad
xiaruidong <xiaruidong_yiqianyinSCM@outlook.es>
2026-09-23 10:24:46 +0200

    BoardTest Created

--- src/test/java/main/BoardTest.java
+++ src/test/java/main/BoardTest.java
@@ -1,4 +1,15 @@
 package main;
 
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 public class BoardTest {
+    @Test
+    public  void creacionBoardTest(){
+        Board board = new Board();
+        assertNotNull(board, "Board should be created succesfully");
+    }
+
 }
